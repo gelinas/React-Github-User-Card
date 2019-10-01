a89214f586b68f22b80312a58e7a8f4c9cf2697b
gelinas <jlgelinas@gmail.com>
2019-10-01 18:47:08 -0400

    MVP minus followers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import UserCard from './components/UserCard'
+
+import './App.scss';
+
+class App extends React.Component {
+  state = {
+    userName: 'gelinas',
+    userUrl: '',
+    userData: [],
+    userFollowersUrl: '',
+    userFollowers: []
+  }
+
+  componentDidMount() {
+    axios
+    .get(`https://api.github.com/users/${this.state.userName}`)
+    .then(res => {
+      // console.log("user url res", res);
+      this.setState({
+        userData: res.data,
+        userFollowersUrl: res.data.followers_url
+      });
+      axios
+        .get(this.state.userFollowersUrl)
+        .then(res =>{
+          // console.log("followers url res", res)
+          this.setState({
+            userFollowers: res.data
+          });
+          console.log("nested axios", this.state)
+          })
+        .catch(err => console.log(err));
+      // console.log(this.state.userData)  
+      // console.log(this.state);
+    })
+    .catch(err => console.log(err));
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            {`${this.state.userData.name} Github User Card`}
+          </h1>
+        </header>
+        <section>
+          <UserCard userData={this.state.userData} />
+        </section>
+        <section>
+          <h2>{`${this.state.userData.name} follower list`}</h2>
+          {}
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
